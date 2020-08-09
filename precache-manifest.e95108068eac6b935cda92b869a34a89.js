a0c1f3dfc09eb2df89f2bc35887f87a04daf845d
Jiban Kumar Debashis Behera <66745177+jibanblog@users.noreply.github.com>
2020-08-09 10:37:24 +0530

    Updates

---
+++ precache-manifest.e95108068eac6b935cda92b869a34a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b545e6d8ab4bae33104c005236dd005e",
+    "url": "/covidapiapp/index.html"
+  },
+  {
+    "revision": "2a7caa3d0a43c2db8022",
+    "url": "/covidapiapp/static/css/main.5d2183af.chunk.css"
+  },
+  {
+    "revision": "59e5b8fa1985f05ca7af",
+    "url": "/covidapiapp/static/js/2.954ae24f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covidapiapp/static/js/2.954ae24f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7caa3d0a43c2db8022",
+    "url": "/covidapiapp/static/js/main.8f4e7971.chunk.js"
+  },
+  {
+    "revision": "26ab72720ccb452d3724",
+    "url": "/covidapiapp/static/js/runtime-main.43ceb20d.js"
+  }
+]);
